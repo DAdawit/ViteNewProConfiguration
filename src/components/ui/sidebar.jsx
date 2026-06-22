d61d97f35182a6af0481df4850f590e263534aa9
Dawit Diriba <dawitccnt@gmail.com>
2026-06-22 15:18:50 +0300

    sticky navbar fix

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -208,7 +208,7 @@ function Sidebar({
       <div
         data-slot="sidebar-container"
         className={cn(
-          "fixed z-10 h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear",
+          "fixed z-10 h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex",
           side === "left"
             ? "group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
             : "group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
